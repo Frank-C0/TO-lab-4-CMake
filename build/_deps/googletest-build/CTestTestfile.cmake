f39717cf1f66f630f396934546ba1bea8fd9aff4
Frank C <84058890+Frank-C0@users.noreply.github.com>
2022-11-02 02:22:26 +0000

    Initial commit

---
+++ build/_deps/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/runner/TO-lab-4-CMake/build/_deps/googletest-src
+# Build directory: /home/runner/TO-lab-4-CMake/build/_deps/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
