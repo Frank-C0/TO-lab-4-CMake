ee416cb78755c5f4fd5f2e2c11ec82821e13ebc7
Frank-C0 <fkanaco@unsa.edu.pe>
2022-11-01 23:04:32 -0500

    build remove

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/gith/TO-lab-4-CMake
-# Build directory: D:/gith/TO-lab-4-CMake/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("D:/gith/TO-lab-4-CMake/build/hello_test[1]_include.cmake")
-subdirs("_deps/googletest-build")
